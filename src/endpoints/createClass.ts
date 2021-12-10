83463c1bad6989b128221c9be4f8d27522702fe7
Tainá Soares <tainasoares10eler@gmail.com>
2021-12-10 11:09:06 -0300

    Endpoints de criar turma e visualizar turmas ativas

---
+++ src/endpoints/createClass.ts
@@ -0,0 +1,29 @@
+import {Request, Response} from "express"
+import connection from "../connection"
+import {Turma} from "../type"
+
+
+const createClass = async(req:Request, res:Response) => {
+    try {
+        const {nome, modulo} = req.body
+
+        if(!nome){
+            throw new Error("Parâmetro faltando!")
+        }
+
+        const turma: Turma = {
+            id:Date.now().toString(),
+            nome,
+            modulo
+        }
+
+        await connection("Turma").insert(turma)
+        res.status(200).send({message: "Turma criada!"})
+        
+    } catch (error:any) {
+        res.status(400).send({message: error.message})
+        
+    }
+}
+
+export default createClass
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,17 @@
 import {Request, Response} from "express"
 import app from "./app"
+import createClass from "./endpoints/createClass"
 import createStudent from "./endpoints/createStudents"
+import getActiveClasses from "./endpoints/getActiveClasses"
 import getStudents from "./endpoints/getStudents"
 
 // Teste
 app.get("/", (req:Request, res:Response) => {
     res.send("Hello World!")
 })
+
+app.get("/class", getActiveClasses)
+app.post("/class", createClass)
 
 app.get("/student", getStudents)
 app.post("/student", createStudent)
